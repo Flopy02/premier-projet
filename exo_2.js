c0103768a7538942cb99d5beff335d0ea6f8e661
flopy02 <flo.barege@gmail.com>
2024-10-05 06:06:41 +0200

    age 21 mis à jour

--- exo_2.js
+++ exo_2.js
@@ -1,10 +1,25 @@
-let fruits = ['pomme', 'banane', 'orange', 'fraise', 'mangue'];
+//let fruits = ['pomme', 'banane', 'orange', 'fraise', 'mangue'];
 
 // Extraire une portion du tableau de l'indice 1 à l'indice 3 (l'indice de fin n'est pas inclus)
-let slicedFruits = fruits.slice(1, 3);
+//let slicedFruits = fruits.slice(1, 3);
 
 // Trouver l'indice de l'élément 'orange'
-let index = fruits.indexOf('orange');
+//let index = fruits.indexOf('orange');
 
-console.log(index); // Affiche: 2
-console.log(slicedFruits); // Affiche: ['banane', 'orange']
+//console.log(index); // Affiche: 2
+//console.log(slicedFruits); // Affiche: ['banane', 'orange']
+
+// Création de l'objet student
+const student = {
+    name: "Marie",
+    age: 20,
+    courses: [], // Tableau vide
+    grade: "A",
+  
+};
+student.age = 21;
+
+
+
+// Affichage de l'objet student dans la console
+console.log(student);
--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 //declarer une variable
-let firstName="Flora"
-let age = 22
-let phrase = "Je m'appelle Flora BAREGE"
+//let firstName="Flora"
+//let age = 22
+//let phrase = "Je m'appelle Flora BAREGE"
 
 //changer ça valeur 
 
-
-
 // afficher la valeur dans la console
-console.log(firstName, age, phrase)
+//console.log(firstName, age, phrase)
+
+//functions
