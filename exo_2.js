6d9b8fc210bf3f4ef6b7f57b967b264431d5d688
flopy02 <flo.barege@gmail.com>
2024-10-18 16:14:27 +0200

    test_td_2

--- exo_2.js
+++ exo_2.js
@@ -1,20 +1,20 @@
 // Création de l'objet student
-const student = {
-    name: "Marie",
-    age: 21,
-    courses: [] // Tableau vide
-};
+//const student = {
+    //name: "Marie",
+   // age: 21,
+   // courses: [] // Tableau vide
+//};
 
 // Ajout des éléments au tableau 'courses' avec la méthode push
-student.courses.push("Math", "Physics", "Chemistry");
+//student.courses.push("Math", "Physics", "Chemistry");
 
 // Utilisation de la méthode indexOf pour trouver l'index de "Physics"
-const physicsIndex = student.courses.indexOf("Physics");
-console.log("Index de Physics : ", physicsIndex);
+//const physicsIndex = student.courses.indexOf("Physics");
+//console.log("Index de Physics : ", physicsIndex);
 
 // Utilisation de la méthode slice pour créer un nouveau tableau avec les deux premiers éléments
-const firstTwoCourses = student.courses.slice(0, 2);
-console.log("Les deux premiers cours : ", firstTwoCourses);
+//const firstTwoCourses = student.courses.slice(0, 2);
+//console.log("Les deux premiers cours : ", firstTwoCourses);
 
 // Affichage de l'objet student et des résultats dans la console
-console.log("Objet student : ", student);
+//console.log("Objet student : ", student);
